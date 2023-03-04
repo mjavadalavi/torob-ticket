2c5c17b02a8764cf2a54b0d595c9149250e027b3
mr-mokhtari <mr.mokhtari97@gmail.com>
2023-03-04 13:22:08 +0330

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('ticket')->group(function () {
+
+    Route::prefix('list')->group(function () {
+        Route::match(['get', 'post'], '/sources', "WeeklyScheduleController@sources")->name('sources');
+        Route::match(['get', 'post'], '/destinations', "WeeklyScheduleController@destinations")->name('destinations');
+        Route::match(['get', 'post'], '/terminals', "WeeklyScheduleController@terminals")->name('terminals');
+    });
+
+    Route::match(['get', 'post'],'/searches', "SearchController@index")->name('searches');
+
+    Route::post('/reserve', 'ReserveController@store')->name("reserve_store");
+    Route::post('/cancellation', 'ReserveController@cancellation')->name("reserve_cancellation");
+
+    Route::post('/buy', 'BuyController@store')->name("buy_store");
+    Route::post('/extradition', 'BuyController@extradition')->name("extradition");
+
+    Route::post('/check', 'BuyController@check')->name("check");
+
 });
