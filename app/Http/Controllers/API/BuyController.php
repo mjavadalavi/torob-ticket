44a61a71770f08f0124c7bedc81aa372e4f77809
mjavadalavi <alonumber@protonmail.com>
2023-03-05 23:01:02 +0330

    fixed bugs of documentation.

--- app/Http/Controllers/API/BuyController.php
+++ app/Http/Controllers/API/BuyController.php
@@ -8,14 +8,6 @@ use Illuminate\Http\Request;
 class BuyController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
