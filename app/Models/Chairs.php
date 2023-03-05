605c2938e0bfe0df952120660d2a6eaa0461839c
mr-mokhtari <mr.mokhtari97@gmail.com>
2023-03-05 11:39:42 +0330

    added doc and fixed relation

--- app/Models/Chairs.php
+++ app/Models/Chairs.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use Carbon\Traits\Date;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Chairs extends Model
 {
@@ -15,5 +15,56 @@ class Chairs extends Model
         'chairs' => "array",
     ];
 
+    public function weekly_schedule() {
+        return $this->belongsTo(WeeklySchedule::class);
+    }
+
+    /**
+     * @OA\Property(
+     *     title="ID",
+     *     description="ID",
+     *     format="int64",
+     *     example=1
+     * )
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @OA\Property(
+     *     title="ID",
+     *     description="ID",
+     *     format="int64",
+     *     example=1
+     * )
+     *
+     * @var integer
+     */
+    private $ws_id;
+
+    /**
+     * @OA\Property(
+     *     title="Chairs",
+     *     description="array of chairs are empty",
+     *     format="array",
+     *     example=1
+     * )
+     *
+     * @var array
+     */
+    private $chairs;
+
+    /**
+     * @OA\Property(
+     *     title="date",
+     *     description="date of teravelling",
+     *     format="date",
+     *     example=1
+     * )
+     *
+     * @var Date
+     */
+    private $date;
 
 }
